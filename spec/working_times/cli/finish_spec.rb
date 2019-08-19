b7c91945f97945cdc11867519faa45f79e609252
arsley <blue20will@gmail.com>
2019-08-19 21:43:05 +0900

    :white_check_mark: Improve spec for WorkingTimes::CLI#finish

--- spec/working_times/cli/finish_spec.rb
+++ spec/working_times/cli/finish_spec.rb
@@ -1,7 +1,60 @@
 # frozen_string_literal: true
 
+require 'fileutils'
+
 RSpec.describe 'WorkingTimes::CLI#finish' do
-  it 'should success' do
-    expect(true).to be_truthy
+  let(:data_dir) { WorkingTimes::Config.data_dir }
+  let(:default_work) { WorkingTimes::Config.default_work }
+  after { FileUtils.rm_rf(WorkingTimes::Config.data_dir) }
+
+  context 'when call without start working' do
+    it 'shows "not started" message' do
+      msg = 'You are not starting work. Execute "wt start" to start working.'
+      expect { WorkingTimes::CLI.new.finish }.to output(msg).to_stdout
+    end
+  end
+
+  context 'when call without comment' do
+    before do
+      WorkingTimes::CLI.new.start
+      WorkingTimes::CLI.new.finish
+    end
+
+    it 'adds record like "finish,,TIMESTAMP"' do
+      label, comment, timestamp = File.readlines("#{data_dir}/#{default_work}").last.chomp.split(',')
+      expect(label).to eq('finish')
+      expect(comment).to be_empty
+      expect(timestamp).not_to be_empty
+    end
+
+    it 'presents 2 records on working file' do
+      expect(File.readlines("#{data_dir}/#{default_work}").size).to eq(2)
+    end
+
+    it 'deletes data_dir/.working' do
+      expect(File.exist?("#{data_dir}/.working")).to be_falsey
+    end
+  end
+
+  context 'when call with comment' do
+    before do
+      WorkingTimes::CLI.new.start
+      WorkingTimes::CLI.new.finish('comment')
+    end
+
+    it 'adds record like "finish,COMMENT,TIMESTAMP"' do
+      label, comment, timestamp = File.readlines("#{data_dir}/#{default_work}").last.chomp.split(',')
+      expect(label).to eq('finish')
+      expect(comment).not_to be_empty
+      expect(timestamp).not_to be_empty
+    end
+
+    it 'presents 2 records on working file' do
+      expect(File.readlines("#{data_dir}/#{default_work}").size).to eq(2)
+    end
+
+    it 'deletes data_dir/.working' do
+      expect(File.exist?("#{data_dir}/.working")).to be_falsey
+    end
   end
 end
